bdf61828ffa78d11e53df3c431e5734ae1d052c4
Denny Sesay <79401100+DennySesay@users.noreply.github.com>
2026-06-09 01:15:18 +0200

    feat(cli): delegate main method execution to Picocli CommandLine in LiveScribeApp

--- src/main/java/com/dennysesay/livescribe/LiveScribeApp.java
+++ src/main/java/com/dennysesay/livescribe/LiveScribeApp.java
@@ -1,30 +1,10 @@
 package com.dennysesay.livescribe;
 
-import com.dennysesay.livescribe.config.AppConfig;
-import com.dennysesay.livescribe.scribe.StreamMonitor;
-
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
+import picocli.CommandLine;
 
 public class LiveScribeApp {
-    public static void main(String[] args) throws IOException {
-        AppConfig config = new AppConfig();
-        StreamMonitor monitor = new StreamMonitor(config);
-
-        monitor.start();
-
-        final var keepAlive = new CountDownLatch(1);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("Shutdown requested, stopping monitor");
-            monitor.stop();
-            keepAlive.countDown();
-        }, "shutdown-hook"));
-
-        try {
-            keepAlive.await();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new LiveScribeCommand()).execute(args);
+        System.exit(exitCode);
     }
 }
